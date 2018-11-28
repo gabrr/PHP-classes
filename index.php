31e6d1c5f64378676d368091ae692415b649e2c9
Gabriel Sôarés <42101525+gabrr@users.noreply.github.com>
2018-11-28 03:05:26 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+        <link rel="icon" href="img/king.jpg">
+		<!--<link rel="stylesheet" type="text/css" href="estilo.css">-->
+		<title>Funny Messages</title>
+	</head>
+
+	<body>
+        <div class="container">
+          <h1>Entry page</h1>
+        </div>
+        
+        <style>
+          .container{
+            width: 100%;
+            height: 100vh;
+            background-color: #000;
+            position: relative;
+          }
+        h2{
+          color: #777;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+        </style>
+    
+</html>
